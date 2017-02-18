ae9f048976c86a2e1248113d80f397ad9338bacf
JPML <jpml1g14@soton.ac.uk>
2017-02-18 00:45:00 +0000

    add customiseable day night cycle, sun lens flare

---
+++ Assets/_Scripts/TimeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+
+    //time speed variables
+    public float minutesPerFrame;
+    public float initialTime;
+    float time;
+
+    //object references
+    public Sunlight sun;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        time = initialTime;
+        Update();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float minute = 1.0F / 60.0F;
+        time += minute * minutesPerFrame;
+        time = time % 24;
+        sun.updateTime(time);
+    }
+}
