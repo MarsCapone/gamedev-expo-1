f624facf23d64171b0a39cfc80033494ad15b85a
Samson Danziger <samson.danziger@gmail.com>
2017-02-18 02:35:44 +0000

    add surrounding terrains and borders

---
+++ Assets/_Scripts/TerrainTiler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainTiler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Terrain terrain = GetComponent<Terrain> ();
+		terrain.SetNeighbors (terrain, terrain, terrain, terrain);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
